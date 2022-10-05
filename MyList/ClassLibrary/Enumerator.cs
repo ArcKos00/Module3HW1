5eb37f84b0a8c2396422747f62ae786c12d8fff8
ArcKos00 <105163313+ArcKos00@users.noreply.github.com>
2022-10-05 13:09:18 +0300

    Methods done
    Methods Resize and Add Done
    Also class Enumerator for enumeration collections

---
+++ MyList/ClassLibrary/Enumerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Enumerator<T> : IEnumerator
+    {
+        private T[] _arr;
+        private int _position = -1;
+        public Enumerator(T[] values)
+        {
+            _arr = values;
+        }
+
+        public T Current
+        {
+            get
+            {
+                if (_position == -1 || _position >= _arr.Length)
+                {
+                    throw new InvalidOperationException();
+                }
+                else
+                {
+                    return _arr[_position];
+                }
+            }
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            if (_position < _arr.Length - 1)
+            {
+                _position++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            _position = -1;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- MyList/ClassLibrary/MyList.cs
+++ MyList/ClassLibrary/MyList.cs
@@ -4,10 +4,88 @@ namespace ClassLibrary
 {
     public class MyList<T> : IEnumerable
     {
-        public IEnumerator GetEnumerator()
+        private T[] _array;
+        private int _index = 0;
+
+        public MyList()
         {
+            _array = new T[0];
         }
 
-        
+        public MyList(int count)
+        {
+            _array = new T[count];
+        }
+
+        public MyList(params T[] collection)
+        {
+            _array = new T[collection.Length];
+            for (int i = 0; i < collection.Length; i++)
+            {
+                _array[i] = collection[i];
+            }
+
+            _index = collection.Length - 1;
+        }
+
+        public T[] GetList
+        {
+            get { return _array; }
+        }
+
+        // пересчет элементов коллекции
+        public IEnumerator GetEnumerator()
+        {
+            return new Enumerator<T>(_array);
+        }
+
+        // Добавить в конец коллекуции элемент
+        public void Add(T value)
+        {
+            if (_array[_index] == null && _index < _array.Length)
+            {
+                _array[_index] = value;
+                _index++;
+            }
+            else
+            {
+                Resize(ref _array, 5);
+                Add(value);
+            }
+        }
+
+        // добавить в конец колекции коллекцию или массив
+        public void AddRange(T[] value)
+        {
+
+        }
+
+        // Удалить из колекции данный элемент и сдвинуть коллекцию в случае успешного удаления вернуть труе
+        public bool Remove(T value)
+        {
+            return true;
+        }
+
+        // удалить из коллекции элемент по индексу и сдвинуть коллекцию
+        public void RemoveAt(int index)
+        {
+
+        }
+
+        // отсортировать коллекцию
+        public void Sort()
+        {
+            Array.Sort();
+        }
+
+        private void Resize(ref T[] arr, int diff = 1)
+        {
+            T[] newArr = new T[arr.Length + diff];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+        }
     }
 }
